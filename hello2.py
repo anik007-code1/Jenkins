9c44259c7fe8c03ab3d984e2da5f92f01e96475a
aniklpu01 <aniklpu01@gmail.com>
2024-04-28 16:24:09 +0600

    jenkins files added

--- hello2.py
+++
@@ -1,6 +0,0 @@
-given_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
-output = []  
-for i in given_list:  
-    if i % 2 == 0:  
-        output.append(i)   
-print("Output List using for loop:", output)
