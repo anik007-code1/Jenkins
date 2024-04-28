f86203c1ea7fa0e4d7398dd14e9490559e54d465
anik007-code1 <153408417+anik007-code1@users.noreply.github.com>
2024-04-28 17:39:06 +0600

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$!j$dqr*=2atu8dugimfko4=a0b3*pveh^63-101nsp59#w$d7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
