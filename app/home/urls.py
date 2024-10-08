aedaa5009bc60f1f73801d478bb2d9402104ef43
aniklpu01 <aniklpu01@gmail.com>
2024-10-08 15:13:06 +0600

    updated

--- app/home/urls.py
+++ app/home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout', views.logout, name="logout"),
+    path('', views.home, name='home')
 ]
+
--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already used.")
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username already Used")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, "Password Not the Same")
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid credentials")
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
